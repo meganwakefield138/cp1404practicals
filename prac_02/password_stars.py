1369f69afe04d1be28382f1c40d87bee6f5773a5
meganwakefield <megan.wakefield@my.jcu.edu.au>
2025-06-07 21:40:43 +1000

    Refactor password check program to use function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -7,9 +7,17 @@ This program asks the user for a password and displays it with asterisks.
 MIN_LENGTH = 8
 
 def main():
-    password = input("Enter password (min. 8 characters): ")
+   password = get_password()
+   print_stars(password)
+
+def get_password():
+    password = input(f"Enter password (min. 8 characters): ")
     while len(password) < MIN_LENGTH:
-        password = input("Password must be at least 8 characters. Enter password again: ")
+        print("Password must be at least 8 characters.")
+        password = input("Enter password again: ")
+    return password
+
+def print_stars(password):
     print("*" * len(password))
 
 main()
