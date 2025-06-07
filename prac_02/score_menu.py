10caa1fafabbe95fbcb2dbc84c7b3abd7540a971
meganwakefield <megan.wakefield@my.jcu.edu.au>
2025-06-07 15:32:02 +1000

    Add score menu program with options

---
+++ prac_02/score_menu.py
@@ -0,0 +1,26 @@
+"""
+score_menu.py
+
+This program allows the user to interact with the score menu.
+Users can enter a valid score (0-100), display the result category, and exit.
+The program demonstrates function reuse and input validation
+"""
+
+def display_menu():
+    """Displays the score menu"""
+    print("Score Menu")
+    choice = input("Enter your choice: >").upper()
+    while choice != "Q":
+        if choice == "G":
+            pass
+        elif choice == "P"
+            pass
+        elif choice == "S"
+            pass
+        else:
+            print("Invalid choice. Please try again.")
+    print("Score Menu")
+    choice = input("Enter your choice: >").upper()
+
+
+    
