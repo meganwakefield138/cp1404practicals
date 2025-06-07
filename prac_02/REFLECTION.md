9422651a81b72e72be07416d54886d3fac27b1e5
meganwakefield <megan.wakefield@my.jcu.edu.au>
2025-06-07 21:47:51 +1000

    Add reflection about practicals

---
+++ prac_02/REFLECTION.md
@@ -0,0 +1,28 @@
+# CP1404 Practical Reflection
+
+Write short but thoughtful answers to each of the following.  
+Replace each `...` with your meaningful answer.
+
+## What percentage of the lecture videos have you watched so far?
+
+I am not sure.
+
+## What time, location and duration have you scheduled to watch lecture videos?
+
+I have not scheduled a time, location or duration to watch lecture videos.
+
+## What are you doing well in the subject so far that you will keep doing?
+
+Asking for help when I need it.
+
+## What do you need to stop or change in your work for this subject overall so far?
+
+I need to stop leaving things to the last minute and start doing things right from the start.
+
+## What are you currently doing poorly for practicals in this subject that you will change?
+
+A poor habit that I have is not reading all instructions before submitting my work.
+
+## What are you doing really well for practicals in this subject?
+
+I try to use all the information taught in the lecture videos to code as best as I can.
--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -7,20 +7,27 @@ MENU = """ C - Convert from Celsius to Fahrenheit
  F - Convert from Fahrenheit to Celsius
  Q - Quit """
 
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
+def main():
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice in ["C", "F"]:
+            convert_temperature(choice)
+        else:
+            print("Invalid choice. Please try again.")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Goodbye!")
+
+def convert_temperature(choice):
     if choice == "C":
         celsius = float(input("Enter temperature in Celsius: "))
         fahrenheit = (celsius * 9 / 5) + 32
-        print(f"{celsius} degrees Celsius is {fahrenheit} degrees Fahrenheit.")
+        print(f"{celsius} degrees Celsius is {fahrenheit:.2f} degrees Fahrenheit.")
     elif choice == "F":
         fahrenheit = float(input("Enter temperature in Fahrenheit: "))
         celsius = (fahrenheit - 32) * 5 / 9
-        print(f"{fahrenheit} degrees Fahrenheit is {celsius} degrees Celsius.")
-    else:
-        print("Invalid choice. Please try again.")
-    print(MENU)
-    choice = input(">>> ").upper()
-print("Goodbye!")
+        print(f"{fahrenheit} degrees Fahrenheit is {celsius:.2f} degrees Celsius.")
+
+main()
 
