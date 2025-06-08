3c550df7aff211f82c17e0633a2ca4a46edaae0b
meganwakefield <megan.wakefield@my.jcu.edu.au>
2025-06-08 18:04:27 +1000

    Format guitar cost with comma and no decimal places using f-string

--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -35,7 +35,7 @@ for i, number in enumerate(numbers, 1):
 # TODO: Use f-string formatting to produce the output:
 # 1922 Gibson L-5 CES for about $16,036!
 
-print(f"{year} {name} for about {cost}")
+print(f"{year} {name} for about ${cost:,.0f}!")
 
 # TODO: Using a for loop with the range function and f-string formatting,
 # produce the following right-aligned output (DO NOT use a list):
